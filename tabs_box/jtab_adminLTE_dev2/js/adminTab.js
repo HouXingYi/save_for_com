ad30fbd42a6757aaaa541e8ad06eceadc9076e22
houxingyi <1026043046@qq.com>
2017-09-26 18:14:29 +0800

    s

---
+++ tabs_box/jtab_adminLTE_dev2/js/adminTab.js
@@ -0,0 +1,44 @@
+
+$(function(){
+    init();
+});
+function init(){
+    $.fn.initJerichoTab({
+        renderTo: '.content-wrapper',
+        uniqueId: 'myJerichoTab',
+        contentCss: { 'height': "100px" },
+        tabs: [{
+            title: 'main',
+            closeable: false,
+            data: { dataType: 'iframe', dataLink: 'template/main.html' }
+        }],
+        activeTabIndex: 0,   //最先激活的tab  // 首页
+        loadOnce: true
+    });
+    bindEvent();
+}
+function bindEvent(){
+    $('span.func').click(function() {
+        $.fn.jerichoTab.addTab({
+            tabFirer: $(this),
+            title: $(this).text(),
+            closeable: true,
+            data: {
+                dataType: $(this).attr('dataType'),
+                dataLink: $(this).attr('dataLink')
+            }
+        })
+        .showLoader().loadData();
+    });
+
+    $(".modules").on("click",function(){
+        var listBox = $(this).siblings(".modulesList");
+        var isHid = listBox.css("display");
+        if( isHid == "block" ){
+            listBox.fadeOut();
+        }else if( isHid == "none" ){
+            listBox.fadeIn();
+        }
+    });
+}
+
